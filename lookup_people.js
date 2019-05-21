b43e5f18efeeb80852c854a2cbc392ef5647eac9
Britta Jorgenson <britta.jorgenson@gmail.com>
2019-05-21 16:25:06 -0600

    refactor lookup_people

--- lookup_people.js
+++ lookup_people.js
@@ -1,45 +1,43 @@
 const pg = require('pg');
 const settings = require('./settings');
-
-const client = new pg.Client({
-  user      : settings.user,
-  password  : settings.password,
-  database  : settings.database,
-  host      : settings.hostname,
-  port      : settings.port,
-  ssl       : settings.ssl
-});
-
+const client = new pg.Client(settings);
 const queryName = process.argv.slice(2)[0];
 
-client.connect((err) => {
+//======= Helper Function to Print People =============
+function printPeople(err, name, people){
+    if (err) {
+        throw err;
+    } else {
+        console.log(`Found ${people.length} person(s) by the name '${name}':`)
+        people.forEach((person, index) => {
+            const birthdate = new Date(person.birthdate)
+                .toISOString()
+                .split('T')[0];
+            console.log(
+                `- ${index + 1}: ${person.first_name} ${person.last_name}, born '${birthdate}'`
+            )
+        });
+    }
+}
 
+//======= Connecting to the Database ==============
+client.connect((err) => {
   if (err) {
     return console.error('Connection Error', err);
   }
   
-
   function getPeople(name, cb) {
-      client.query('SELECT * FROM famous_people WHERE first_name = $1 OR last_name = $1', [name], (err, res) => {
+      client.query(`SELECT * FROM famous_people 
+                    WHERE first_name = $1 OR last_name = $1`, 
+                    [name], (err, res) => {
         if (err) {
-            return console.error('error running query', err);
+            throw err;
         }
-        cb(err, name, res.rows)
-        client.end();
+            cb(null, name, res.rows)
+            client.end();
         });
     }
 
-  getPeople(queryName, (err, name, people) => {
-    if (err) {
-        console.log('Error:', err)
-    } else {
-        console.log(`Found ${people.length} person(s) by the name '${name}':`)
-        people.forEach((person, index) => {
-            const birthdate = new Date(person.birthdate).toISOString().split('T')[0];
-            console.log(`- ${index + 1}: ${person.first_name} ${person.last_name}, born '${birthdate}'`)
-        })
-    }
-})
-
+    getPeople(queryName, printPeople);
 });
 
